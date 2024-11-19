c78fd5fb1b69bcf2f266787d11dfe857bbeefc6e
st4rhub <oceanepioche@gmail.com>
2024-11-19 11:23:56 +0100

    Split dataset using scikit-learn

--- script.py
+++ script.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from visualization import vizualise_parameter
 import os
 
 df = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_ca_features_85(1).csv")
+df_values = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_ca_labels_85.csv")
 
-for col in df.columns.tolist() :
-    vizualise_parameter(str(col))
+#for col in df.columns.tolist() :
+    #vizualise_parameter(str(col))
 
+#Decide the train and test sets
+x_train,x_test,y_train,y_test = train_test_split(df, 
+df_values,test_size = 0.3, shuffle=True)
+
+print(y_train.head())
