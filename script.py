d9e738e8d11404ae12c6e8753da5699aca7b197d
st4rhub <oceanepioche@gmail.com>
2024-11-19 11:42:57 +0100

    Scale data using scikit-learn

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from visualization import vizualise_parameter
 import os
 
+scaler = StandardScaler()
 df = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_ca_features_85(1).csv")
 df_values = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_ca_labels_85.csv")
 
@@ -13,5 +15,15 @@ df_values = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_c
 #Decide the train and test sets
 x_train,x_test,y_train,y_test = train_test_split(df, 
 df_values,test_size = 0.3, shuffle=True)
+print("Train set before scaling : ")
+print(x_train.head())
 
-print(y_train.head())
+x_train_scaled = scaler.fit_transform(x_train)
+
+#Pas de scaler sur le x_test, juste transformer le x_test
+x_test_scaled = scaler.transform(x_test)
+
+print("Train set after scaling : ")
+print(x_train_scaled)
+print("Test set after transformation : ")
+print(x_test_scaled)
