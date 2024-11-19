e94b8378924c51be8dbc426980d5a7659fa85cd2
Binary-Human <mll.sara.bakri@gmail.com>
2024-11-19 10:42:36 +0100

    data analysis automatisation

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
+from visualization import vizualise_parameter
 import os
 
 df = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_ca_features_85(1).csv")
 
-# 1. Basic Summary Statistics
-print(df.describe())
+for col in df.columns.tolist() :
+    vizualise_parameter(str(col))
+
--- visualization.py
+++ visualization.py
@@ -1,13 +1,14 @@
 
 import matplotlib.pyplot as plt 
 import pandas as pd
-x = [] 
-y = [] 
-PARAMETER = "WKHP"
-columns = [PARAMETER]
 
-df = pd.read_csv("alt_acsincome_ca_features_85(1).csv", usecols=columns)
-plt.hist(df, color = 'lightgreen', ec='black', bins=20, label = PARAMETER) 
-plt.xlabel('Distribution of '+PARAMETER) 
-plt.legend() 
-plt.show() 
+def vizualise_parameter(PARAMETER) :
+    x = [] 
+    y = [] 
+    columns = [PARAMETER]
+
+    df = pd.read_csv("Jeu de données 1 - Californie-20241119/alt_acsincome_ca_features_85(1).csv", usecols=columns)
+    plt.hist(df, color = 'lightgreen', ec='black', bins=20, label = PARAMETER) 
+    plt.xlabel('Distribution of '+PARAMETER) 
+    plt.legend() 
+    plt.show() 
