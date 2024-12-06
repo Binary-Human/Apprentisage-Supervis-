a83f27a35f49a6a8f030f543fdd64c449f4a7b5f
Binary-Human <mll.sara.bakri@gmail.com>
2024-12-06 13:42:02 +0100

    refactored metric printing, Started adaBoost

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, classification_report, ConfusionMatrixDisplay
 from visualization import vizualise_parameter
 import joblib
@@ -46,75 +47,8 @@ param_grid = {
     'criterion' :['gini']
 }
 
-# Standard performance metrics
-def standard():
-    rf = RandomForestClassifier()
-    rf.fit(x_train_scaled, y_train )
-    y_pred_std = rf.predict(x_test_scaled)
+def printMetrics(y_test, y_pred):
 
-    print("\nMetrics for Standard parameters\n")
-    #Accuracy : % of correct predictions
-    accuracy = accuracy_score(y_test,y_pred_std)
-    #Precision : % of TP over "predicted as positive"
-    precision = precision_score(y_test, y_pred_std)
-    #Recall : % of TP over "really positive"
-    recall = recall_score(y_test, y_pred_std)
-    print("Accuracy : ", accuracy)
-    print("Precision : ", precision)
-    print("Recall : ", recall)
-
-    # Create the confusion matrix
-    cm = confusion_matrix(y_test, y_pred_std)
-    print("Confusion matrix : ")
-    print(cm)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot()
-    plt.show()
-
-
-# Uses crossValidation to asses performance
-def standardCrossValidation():
-    # Paramétrage par défaut
-    model = RandomForestClassifier()
-    # Obtenir les prédictions avec validation croisée
-    y_pred = cross_val_predict(model, x_test_scaled, y_test, cv=5)
-    # Calculer les scores de validation croisée (accuracy)
-    cv_accuracy = cross_val_score(model, x_test_scaled, y_test, cv=5, scoring='accuracy')
-    cv_precision = cross_val_score(model, x_test_scaled, y_test, cv=5, scoring='precision')
-    cv_recall = cross_val_score(model, x_test_scaled, y_test, cv=5, scoring='recall')
-
-    # Afficher les résultats
-    print("\nMetrics for Standard parameters - Cross validation\n")
-    print("Accuracy moyenne (validation croisée) :", cv_accuracy.mean())
-    print("Précision moyenne (validation croisée) :", cv_precision.mean())
-    print("Recall moyen (validation croisée) :", cv_recall.mean())
-
-    print("\nClassification Report :")
-    print(classification_report(y_test, y_pred))
-
-
-    # Create the confusion matrix
-    cm = confusion_matrix(y_test, y_pred)
-    print("Confusion matrix : ")
-    print(cm)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot()
-    plt.show()
-
-def searchBestModel():
-    rf = RandomForestClassifier()
-    CV_rf = GridSearchCV(estimator=rf, param_grid=param_grid, cv= 5)
-    CV_rf.fit(x_train, y_train)
-    best_params = CV_rf.best_params_
-
-    rf_best = RandomForestClassifier(random_state=None,max_features=best_params["max_features"],
-                                        n_estimators=best_params["n_estimators"],max_depth=best_params["max_depth"],
-                                        criterion=best_params["criterion"])
-
-    rf_best.fit(x_train_scaled,y_train)
-    y_pred = rf_best.predict(x_test)
-
-    # Metrics
     #Accuracy : % of correct predictions
     accuracy = accuracy_score(y_test,y_pred)
     #Precision : % of TP over "predicted as positive"
@@ -133,4 +67,66 @@ def searchBestModel():
     disp.plot()
     plt.show()
 
-standard()
+# Standard performance metrics
+def standard(model):
+    model.fit(x_train_scaled, y_train )
+    y_pred_std = model.predict(x_test_scaled)
+
+    print(f"\nMetrics for {model} parameters\n")
+    printMetrics(y_test, y_pred_std)
+
+# Uses crossValidation to asses performance
+def standardCrossValidation(model):
+    # Obtenir les prédictions avec validation croisée
+    y_pred = cross_val_predict(model, x_test_scaled, y_test, cv=5)
+    # Calculer les scores de validation croisée (accuracy)
+    cv_accuracy = cross_val_score(model, x_test_scaled, y_test, cv=5, scoring='accuracy')
+    cv_precision = cross_val_score(model, x_test_scaled, y_test, cv=5, scoring='precision')
+    cv_recall = cross_val_score(model, x_test_scaled, y_test, cv=5, scoring='recall')
+
+    # Afficher les résultats
+    print("\nMetrics for Standard parameters - Cross validation\n")
+    print("Accuracy moyenne (validation croisée) :", cv_accuracy.mean())
+    print("Précision moyenne (validation croisée) :", cv_precision.mean())
+    print("Recall moyen (validation croisée) :", cv_recall.mean())
+
+    print("\nClassification Report :")
+    print(classification_report(y_test, y_pred))
+
+    # Create the confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion matrix : ")
+    print(cm)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+
+def searchBestRandomForest():
+    model = RandomForestClassifier()
+    CV_rf = GridSearchCV(estimator=model, param_grid=param_grid, cv= 5)
+    CV_rf.fit(x_train, y_train)
+    best_params = CV_rf.best_params_
+
+    rf_best = RandomForestClassifier(random_state=None,max_features=best_params["max_features"],
+                                        n_estimators=best_params["n_estimators"],max_depth=best_params["max_depth"],
+                                        criterion=best_params["criterion"])
+
+    rf_best.fit(x_train_scaled,y_train)
+    y_pred = rf_best.predict(x_test)
+
+    print("\nMetrics for Random Forest parameters\n")
+    printMetrics(y_test, y_pred)
+
+
+def adaBoost():
+    aBoost = AdaBoostClassifier()
+    adaBoost.fit(x_train_scaled, y_train )
+    y_pred_std = adaBoost.predict(x_test_scaled)
+
+#standard(RandomForestClassifier())
+#standardCrossValidation(RandomForestClassifier())
+
+standard(AdaBoostClassifier())
+standardCrossValidation(AdaBoostClassifier())
+
+
